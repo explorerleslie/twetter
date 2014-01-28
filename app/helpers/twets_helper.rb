7b62fa21724ef1411c87dd77b999108c017b109a
Leslie Gerhat <explorerleslie@gmail.com>
2014-01-28 20:08:24 +0000

    Mentions are linked from twets/index and users/show.

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -1,3 +1,15 @@
 module TwetsHelper
+    
+  def format_mentions(twet)
+    twet.content.gsub(/(?<start>[>|\s])@(?<username>(\w+))/, '\k<start><a href="/\k<username>">@\k<username></a>')
+    
+    # twet_split = twet.content.split(" ")
+    # twet_split.each do |word|
+    #   if word == /@(\S+)/
+    #     link_to('@' + word, word)
+    #   end
+    # twet_split.join(" ")
+    
+  end   
   
 end
