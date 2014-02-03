25938ff82289e38ab96d3ed3b7652edade5acae8
Leslie Gerhat <explorerleslie@gmail.com>
2014-02-03 13:29:11 +0000

    Remove old version of format_mentions

--- app/helpers/twets_helper.rb
+++ app/helpers/twets_helper.rb
@@ -2,14 +2,6 @@ module TwetsHelper
     
   def format_mentions(twet_content)
     twet_content.gsub(/(?<start>[>|\s])@(?<username>(\w+))/, '\k<start><a href="/\k<username>">@\k<username></a>')
-    
-    # twet_split = twet.content.split(" ")
-    # twet_split.each do |word|
-    #   if word == /@(\S+)/
-    #     link_to('@' + word, word)
-    #   end
-    # twet_split.join(" ")
-    
   end   
   
 end
