880320afef4d0986363528e36f96502d989e6168
Leslie Gerhat <explorerleslie@gmail.com>
2014-02-04 01:17:44 +0000

    Modify secret_token.rb

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,18 @@
 module UsersHelper
 
+  def get_hash(email_address)
+    email_address = email_address.downcase.strip
+    email_hash = Digest::MD5.hexdigest(email_address)
+  end
+  
+  def gravatar_img(email_address)
+    email_hash = get_hash(email_address)
+    image_src = "http://www.gravatar.com/avatar/#{email_hash}"
+  end
+  
+  def gravatar_profile(email_address)
+    email_hash = get_hash(email_address)
+    profile_link = "http://www.gravatar.com/#{email_hash}"
+  end
+  
 end
